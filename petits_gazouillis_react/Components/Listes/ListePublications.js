1b4f098d3090197afd8e1e74086ebbd681d3da33
fasanterre <fasanterre@hotmail.com>
2020-12-05 01:25:16 -0500

    un esti de fix

--- petits_gazouillis_react/Components/Listes/ListePublications.js
+++ petits_gazouillis_react/Components/Listes/ListePublications.js
@@ -11,18 +11,24 @@ export default class ListePublications extends React.Component{
     
     render(){
         var listUser = []
-        listUser.push(this.props.utilisateurs)
-        return(
-            <View>
-            <FlatList style={{marginTop: 12, flex: 1}} data={this.props.publications} renderItem={({item}) =>
-                <View style={Projet.styles.flexbox}>
-                    <Text style={Projet.styles.flash}>ID: {item.id}</Text>
-                    {listUser.map(util => util.items.map(info => info.id == item.utilisateur_id ? <View style={Projet.styles.flexbox}><Image style={Projet.styles.miniAvatar} source={info.avatar} ></Image> <Text style={Projet.styles.flash}>{info.nom}</Text></View> : <Text></Text>))}
-                    <View>
-                        <Text>dit: {item.corps}</Text>
-                    </View>
-                </View>}
-            /></View>
-        )
+        if(this.props.utilisateurs != null && this.props.utilisateurs != Projet.etiquettes.ENCHARGEMENT){
+            listUser.push(this.props.utilisateurs)
+            return(
+                <View>
+                <FlatList style={{marginTop: 12, flex: 1}} data={this.props.publications} renderItem={({item}) =>
+                    <View style={Projet.styles.flexbox}>
+                        <Text style={Projet.styles.flash}>ID: {item.id}</Text>
+                        {listUser.map(util => util.items.map(info => info.id == item.utilisateur_id ? <View style={Projet.styles.flexbox}><Image style={Projet.styles.miniAvatar} source={info.avatar} ></Image> <Text style={Projet.styles.flash}>{info.nom}</Text></View> : <Text></Text>))}
+                        <View>
+                            <Text>dit: {item.corps}</Text>
+                        </View>
+                    </View>}
+                /></View>
+            )
+        }else{
+            return(
+                <Text>Erreur1</Text>
+            )
+        }
     }
 }
